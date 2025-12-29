3623b1e4863df1424c8fdf5cd8c674ec79eb951e
Trần Quốc Tuấn <tranquoctuantqd2002@gmail.com>
2025-12-29 13:50:55 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { 
+  images: {
     domains: ["assets.kltn.lol"],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://backend.kltn.lol/api/:path*',
+        source: "/api/:path*",
+        destination: "https://backend.kltn.lol/api/:path*",
       },
     ];
   },
-};
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+} as NextConfig;
 
 export default nextConfig;
